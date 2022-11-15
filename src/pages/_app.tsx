70de8f8b61799111c5c28c5f66b1965271912c48
LaercioMLB <lbubiak@hotmail.com>
2022-11-15 15:46:25 -0300

    errei aqui familia, peço perdão

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import type { AppProps } from 'next/app';
+import { ReactElement } from 'react';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { ThemeProvider } from '@emotion/react';
+import { theme, globalStyle } from '../global/theme';
+import { GlobalStyles, StyledEngineProvider } from '@mui/material';
+
+export default function App({ Component, pageProps }: AppProps): ReactElement {
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <GlobalStyles styles={globalStyle} />
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
+}
