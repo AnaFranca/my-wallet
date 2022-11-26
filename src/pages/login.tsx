77ab7e29db2de6d0381c2de512ca866b32453da6
LaercioMLB <lbubiak@hotmail.com>
2022-11-26 09:12:34 -0300

    feat-refeito-tela-login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,18 +1,44 @@
+/* eslint-disable no-useless-escape */
+/* eslint-disable prefer-regex-literals */
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import { ReactElement } from 'react';
+import { ReactElement, useCallback } from 'react';
 import { Button, Container, Stack, TextField, Typography } from '@mui/material';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { CopyrightOutlined } from '@mui/icons-material';
 import Head from 'next/head';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 export default function Login(): ReactElement {
   const { push } = useRouter();
 
-  const handleLogin = async (): Promise<boolean> => {
-    return await push('/');
-  };
+  const handleSubmit = useCallback(async (): Promise<void> => {
+    await push('/');
+  }, [push]);
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .required('Este campo é necessário')
+      .email('Digite um e-mail válido'),
+    password: Yup.string()
+      .required('Este campo é necessário')
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+        'Deve conter 8 caracteres, uma letra maiscula, uma minuscula, um número e um caractere especial',
+      ),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: handleSubmit,
+    validationSchema,
+  });
 
   return (
     <Container maxWidth="sm">
@@ -28,13 +54,39 @@ export default function Login(): ReactElement {
           <Typography mt={2} variant="h4" color="primary" fontWeight="700">
             Login
           </Typography>
-          <TextField variant="outlined" name="email" label="E-mail" fullWidth />
-          <TextField
-            variant="outlined"
-            name="password"
-            label="Senha"
-            fullWidth
-          />
+          <Stack>
+            <TextField
+              variant="outlined"
+              name="email"
+              label="E-mail"
+              fullWidth
+              type="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              error={!!formik.errors.email}
+            />
+
+            <Typography mt={1} variant="caption" color="error">
+              {formik.errors.email}
+            </Typography>
+          </Stack>
+
+          <Stack>
+            <TextField
+              variant="outlined"
+              name="password"
+              label="Senha"
+              fullWidth
+              type="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              error={!!formik.errors.password}
+            />
+
+            <Typography mt={1} variant="caption" color="error">
+              {formik.errors.password}
+            </Typography>
+          </Stack>
 
           <Link href="/forgot-password">
             <Typography onClick={() => console.log('Clicked')}>
@@ -46,7 +98,7 @@ export default function Login(): ReactElement {
             variant="contained"
             size="large"
             fullWidth={false}
-            onClick={handleLogin}
+            onClick={() => formik.handleSubmit()}
           >
             Login
           </Button>
